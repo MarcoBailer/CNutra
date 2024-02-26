8599b678ad2d9ad3facfb46e0b09e84c49e10b70
Marco-willian <marcowcandido@gmail.com>
2024-02-26 10:16:26 -0300

    refactor

--- Nutricao/Core/Interfaces/IFoodCalc.cs
+++ Nutricao/Core/Interfaces/IFoodCalc.cs
@@ -8,10 +8,11 @@ namespace Nutricao.Core.Interfaces
     public interface IFoodCalc 
     {
         Task<FoodServiceResponseDto> CadastrarVariasRef([FromBody] CreateRefeicaoDto refeicao, string nomes);
-        Task<CalculoDaRefeicao> CalculoTotal([FromBody] ReadRefeicaoDto refeicao);
+        Task<FoodServiceResponseDto> CalculoTotal([FromBody] ReadRefeicaoDto refeicao);
         Task<FoodServiceResponseDto> CalcularTotalRefeicaoPelaPosicao([FromQuery] ReadRefeicaoDto refeicao, int lugar);
         Task<List<RefeicaoMVN>> GetRefeicao([FromQuery] ReadRefeicaoDto refeicao);
         Task<List<RefeicaoMVN>> GetRefeicaoByPlace([FromQuery] ReadRefeicaoDto refeicao, int lugar);
+        Task<CalculoDaRefeicao> GetCalculoRefeicao([FromQuery] ReadRefeicaoDto refeicao);
         Task<FoodServiceResponseDto> RemoveRefeicao([FromQuery] ReadRefeicaoDto refeicao,string nome);
         Task<FoodServiceResponseDto> UpdateRefeicao([FromQuery] ReadRefeicaoDto refeicao, [FromBody] UpdateRefeicaoDto updt);
         Task<FoodServiceResponseDto> UpdateRefeicaoDate([FromQuery] ReadRefeicaoDto refeicao, [FromBody] UpdateRefeicaoDto updt);
