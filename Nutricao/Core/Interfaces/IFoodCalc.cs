c336751be2bd82e1b1808a02d2e53d8de15ebf0c
Marco-Willian <marcowcandido@gmail.com>
2024-03-23 18:58:54 -0300

    Encontrar por turno

--- Nutricao/Core/Interfaces/IFoodCalc.cs
+++ Nutricao/Core/Interfaces/IFoodCalc.cs
@@ -10,9 +10,11 @@ namespace Nutricao.Core.Interfaces
         Task<FoodServiceResponseDto> CadastrarVariasRef([FromBody] RefeicaoMVN refeicao);
         Task<FoodServiceResponseDto> CalculoTotal([FromBody] RefeicaoQuery refeicao);
         Task<FoodServiceResponseDto> CalcularTotalRefeicaoPelaPosicao([FromQuery] RefeicaoQuery refeicao, int lugar);
+        Task<FoodServiceResponseDto> CalcularTotalRefeicaoPeloTurno([FromQuery] RefeicaoQuery refeicao, bool mat, bool vesp, bool not);
         Task<List<RefeicaoMVN>> GetRefeicao([FromQuery] RefeicaoQuery refeicao);
         Task<List<RefeicaoMVN>> GetRefeicaoByPlace([FromQuery] RefeicaoQuery refeicao, int lugar);
         Task<CalculoDaRefeicao> GetCalculoRefeicao([FromQuery] RefeicaoQuery refeicao);
+        Task<List<RefeicaoMVN>> GetCalculoRefeicaoTurno([FromQuery] RefeicaoQuery refeicaoQr, bool isMatinal, bool isVespertina, bool isNoturna);
         Task<FoodServiceResponseDto> RemoveRefeicao([FromQuery] RefeicaoQuery refeicao,string nome);
         Task<FoodServiceResponseDto> UpdateRefeicao([FromQuery] RefeicaoQuery refeicao, string nome, string nomeUpdt);
         Task<FoodServiceResponseDto> UpdateRefeicaoDate([FromQuery] RefeicaoQuery refeicao, [FromBody] UpdateRefeicaoDto updt);
